e68ebf6702ecb77628ee05d30e782f7e6eebfe6c
Christophe Deissenberg <keex_e@yahoo.com>
2023-05-29 13:39:41 +0200

    -rien compris

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+# app/controller/pages_controller.rb
+class PagesController < ApplicationController
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Generic syntax:
+  # verb "path", to: "controller#action"
+  get "ask", to: "questions#ask"
+  get "answer", to: 
 end
